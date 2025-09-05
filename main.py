05f60255e1c3c9a78e252e5ab444bd5ffb212bdb
student1 <student1@class.local>
2025-09-05 09:13:54 +0900

    Добавили второе число к игре

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import random
-a = random.randint(10, 15)
+a = random.randint(10, 10)
+c = random.randint(10, 10)
 b = int(input("Введите ваше число: "))
-if (b == a):
-    print("Вы угадали число!")
+d = int(input("Введите ваше число: "))
+if (b == a and c == d):
+    print("Вы угадали числа!")
+elif ((b == a and c != d) or (b != a and c == d)):
+    print("Вы угадали одно из чисел.")
 else:
-    print("Вы не угадали число! Правильное число:", a)
+    print("Вы не угадали числа! Правильные числа:", a," и ", c)
