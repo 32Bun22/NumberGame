ddc362d79d5e820379f1df5c29c53d71c8cf980a
student1 <student1@class.local>
2025-09-12 10:16:00 +0900

    a

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from faker import Faker
 import random
+fake = Faker()
 a = random.randint(10, 10)
 b = int(input("Введите ваше число: "))
 if (b == a):
     print("Вы угадали число!")
 else:
-    print("Вы не угадали число! Правильное число:", a,".")
+    print("Вы не угадали число! Правильное число:", a, ".")
+for _ in range(10):
+    print(fake.name())
+print(fake.address())
 
-#TODO: Добавить проверку на пустой ввод
+nikita = ("никита")
+
+# TODO: Добавить проверку на пустой ввод
